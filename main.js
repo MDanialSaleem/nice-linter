0d4b3f548689c6b56cb899b72aeac5559cc5ff25
Danial Saleem <danialsaleem2010@gmail.com>
2021-09-21 21:59:03 +0500

    feat: adding a very basic parsing mechanism that implements the var to let rule

---
+++ main.js
@@ -0,0 +1,26 @@
+const espree = require("espree");
+
+// in reality this would come from some kind of file.
+const oldSourceCode = "var foo = 1";
+const newSourceCode = "let foo = 1";
+
+const parserOptions = { ecmaVersion: 6 };
+const oldAst = espree.parse(oldSourceCode, parserOptions);
+const newAst = espree.parse(newSourceCode, parserOptions);
+
+let varFound = false;
+let letFound = false;
+for (let node of oldAst.body) {
+  if (node.type == "VariableDeclaration" && node.kind == "var") {
+    varFound = true;
+  }
+}
+for (let node of newAst.body) {
+  if (node.type == "VariableDeclaration" && node.kind == "let") {
+    letFound = true;
+  }
+}
+
+if (varFound && letFound) {
+  console.log("Good work!You converted var to let.");
+}
